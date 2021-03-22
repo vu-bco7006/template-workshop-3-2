57ddbf699b2b8efd764b5242e351c34354917e07
Patrick Pang <mail@patrickpang.net>
2021-03-22 16:57:10 +1100

    First commit.

---
+++ print.py
@@ -0,0 +1,10 @@
+
+student1_name = "Peter Chen"
+student2_name = "John Smith"
+student3_name = "Mary Roberts"
+
+team_name = "Awesome!"
+
+print("Hello team", team_name)
+
+# print("Your team has members:")
